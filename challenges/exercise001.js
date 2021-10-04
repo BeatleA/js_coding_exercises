403b542c737eec6e6a927d6183544344adad0394
Anna W <tester.kodilla@gmail.com>
2021-10-04 21:21:35 +0100

    Undone Prettier changes

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -6,11 +6,7 @@ function capitalize(word) {
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  return (
-    firstName.substring(0, 1).toUpperCase() +
-    "." +
-    lastName.substring(0, 1).toUpperCase()
-  );
+  return (firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1).toUpperCase());
 }
 
 function addVAT(originalPrice, vatRate) {
@@ -49,7 +45,7 @@ function reverseAllWords(words) {
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   let linuxCounter = 0;
-  users.forEach((user) => { if (user.type === "Linux") linuxCounter++; });
+  users.forEach(user => { if (user.type === "Linux") linuxCounter++; });
   return linuxCounter;
 }
 
@@ -58,7 +54,7 @@ function getMeanScore(scores) {
   const length = scores.length;
   if (length !== 0) {
     let mean = 0;
-    scores.forEach((score) => (mean += score));
+    scores.forEach(score => mean += score);
     mean /= length;
     return Math.round(mean * 100) / 100;
   } else {
@@ -87,5 +83,5 @@ module.exports = {
   reverseAllWords,
   countLinuxUsers,
   getMeanScore,
-  simpleFizzBuzz,
+  simpleFizzBuzz
 };
